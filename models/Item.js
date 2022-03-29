2e6feebd947aaefd8ea992f4f7fd142064add628
lrodenyoder <rodenlau000@gmail.com>
2022-03-28 20:57:07 -0500

    initial item model

---
+++ models/Item.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Item extends Model{ }
+
+Item.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        item_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        item_info: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'item'
+    }
+)
