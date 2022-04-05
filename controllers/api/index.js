d6467d04a74284226c3033bbf9f308352b7ca5cc
patrickaregan <patrickaregan@gmail.com>
2022-04-04 20:01:48 -0500

    add schedule routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const itemRoutes = require('./item-routes.js');
+const scheduleRoutes = require('./schedule-routes.js');
 
 // Use middleware
 router.use('/users', userRoutes);
 router.use('/items', itemRoutes);
+router.use('/schedules', scheduleRoutes);
 
 // Export variables
 module.exports = router;
