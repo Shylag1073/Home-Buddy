84cc661f5f66c6058f98f066b2fb9dac18cc6f5b
shylag1073 <shylag1073@gmail.com>
2022-04-05 20:57:43 -0500

    ReadMe

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
 // Define global variables
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const scheduleRoutes = require('./Schedule-routes');
 
 // Use middleware
 router.use('/users', userRoutes);
-router.use('/Schedule', ScheduleRoutes);
+router.use('/schedule', scheduleRoutes);
 
 // Export variables
 module.exports = router;
