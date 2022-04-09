cbfdf47ddfe339b607d9b018e9a36457f647d918
patrickaregan <patrickaregan@gmail.com>
2022-04-09 11:29:26 -0500

    update main with group code

---
+++ public/javascript/signup.js
@@ -0,0 +1,30 @@
+// Define function for handler
+async function signupHandler(event) {
+    // Prevent default behavior
+    event.preventDefault();
+
+    // Define global variables
+    const email = document.querySelector('#signinEmailID').value.trim();
+    const password = document.querySelector('#signinPasswordID').value.trim();
+
+    // Logic
+    if (email && password) {
+        const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+            email,
+            password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+// Add event listener
+document.querySelector('#signupForm').addEventListener('submit', signupHandler);
